8a1cb8b16429fb9236d4f0b0d71bcd7ea87e9233
ph0m1 <2732706745@qq.com>
2023-12-25 20:42:57 +0800

    update 2023-12-25

--- my_ls/mtls.c
+++
@@ -1,6 +0,0 @@
-#include "myls.h"
-
-int main(int argc, char *argv[])
-{
-    
-}
--- my_ls/myls.h
+++ my_ls/myls.h
@@ -1,2 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <limits.h>
+#include <ftw.h>
+#include <errno.h>
+#include <string.h>
+#include <dirent.h>
+
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 800
+#endif
+// void parameter_handing(int argc,char *argv);
+void do_a();
+void do_l();
+void do_R();
+void do_t();
+void do_r();
+void do_i();
+void do_s();
