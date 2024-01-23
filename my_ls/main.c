c1cbd2b1d3effe0f2ada8211f8962af6d7550dd1
Ph0m1 <149316396+Ph0m1@users.noreply.github.com>
2024-01-23 22:56:57 +0800

    Delete my_ls directory

--- my_ls/main.c
+++
@@ -1,9 +0,0 @@
-#include "myls.h"
-int Vec = 0;
-int main(int argc, char *argv[])
-{
-    SqDir dn;
-    dn = (SqDir)malloc(sizeof(DDir));
-    dn->dirlen = 0;
-    Vec = parameter_handing(argc, argv, dn);
-}
